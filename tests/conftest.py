e6db120b383aee2e60d9197f505218571337016b
Ricardo <ricardofrodrigues.contato@gmail.com>
2026-07-28 22:52:36 -0300

    Correcao exericio

--- tests/conftest.py
+++ tests/conftest.py
@@ -32,9 +32,12 @@ def session():
 def _mock_db_time(*, model, time=datetime(2025, 5, 20)):
 
     def fake_time_hook(mapper, connection, target):
-        if hasattr(target, 'created_at') or hasattr(target, 'updated_at'):
+        if hasattr(target, 'created_at'):
             target.created_at = time
 
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+            
     event.listen(model, 'before_insert', fake_time_hook)
 
     yield time
